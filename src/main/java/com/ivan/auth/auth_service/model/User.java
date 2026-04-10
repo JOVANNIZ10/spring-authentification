4f2baffde47f3f209f1ed3f66e12425f3cd26824
JOVANNIZ10 <ipriala37@gmail.com>
2026-04-10 15:05:38 +0200

    feat: User.java

---
+++ src/main/java/com/ivan/auth/auth_service/model/User.java
@@ -0,0 +1,29 @@
+package com.ivan.auth.auth_service.model;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data @NoArgsConstructor @AllArgsConstructor @Builder
+public class User {
+
+    @Id @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "email", nullable = false, unique = true, length = 100)
+    private String email;
+
+    @Column(name = "password", nullable = false, length = 60)
+    private String password;
+    
+}
